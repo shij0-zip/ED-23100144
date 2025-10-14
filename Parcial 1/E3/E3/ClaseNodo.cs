35f69037429cc1f0d3f0bda1d03447f8e6476221
shij0-zip <josueal.cass@gmail.com>
2025-10-14 09:42:45 -0600

    Ejercicio 3 agregado

---
+++ Parcial 1/E3/E3/ClaseNodo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3
+{
+    internal class ClaseNodo<Tipo>
+    {
+        private Tipo _objetoConDatos;
+        private ClaseNodo<Tipo> _siguiente;
+
+        public Tipo ObjetoConDatos
+        {
+            get { return _objetoConDatos; }
+            set { _objetoConDatos = value; }
+        }
+        public ClaseNodo<Tipo> Siguiente
+        {
+            get { return _siguiente; }
+            set { _siguiente = value; }
+        }
+        ~ClaseNodo()
+        {
+            ObjetoConDatos = default(Tipo);
+        }
+    }
+}
+
---
+++ Parcial 1/E3/E3/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3
+{
+    class ClasePilaDinamica<Tipo> where Tipo : IEquatable<Tipo>
+    {
+        private ClaseNodo<Tipo> _Top;
+        public ClasePilaDinamica()
+        {
+            Top = null;
+        }
+        private ClaseNodo<Tipo> Top
+        {
+            get { return _Top; }
+            set { _Top = value; }
+        }
+        public bool Vacia
+        {
+            get
+            {
+                if (Top == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public void Push(Tipo objeto)
+        {
+            if (Vacia)
+            {
+                ClaseNodo<Tipo> nodoNuevo = new ClaseNodo<Tipo>();
+                nodoNuevo.ObjetoConDatos = objeto;
+                nodoNuevo.Siguiente = null;
+                Top = nodoNuevo;
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                nodoActual = Top;
+                do
+                {
+                    if (objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        throw new Exception("No se permiten datos duplicados");
+                    }
+                    else
+                    {
+                        nodoPrevio = nodoActual;
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                } while (nodoActual != null);
+
+                ClaseNodo<Tipo> nodoNuevo = new ClaseNodo<Tipo>();
+                nodoNuevo.ObjetoConDatos = objeto;
+                nodoNuevo.Siguiente = Top;
+                Top = nodoNuevo;
+            }
+        }
+        public Tipo Pop(Tipo objeto)
+        {
+            if (Vacia)
+            {
+                throw new Exception("La lista se encuentra vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                Tipo Auxiliar;
+                nodoActual = Top;
+                nodoPrevio = Top;
+                do
+                {
+                    if (objeto.Equals(Top.ObjetoConDatos))
+                    {
+                        Auxiliar = nodoActual.ObjetoConDatos;
+                        Top = nodoActual.Siguiente;
+                        nodoActual = null;
+                        return Auxiliar;
+                    }
+                    if (objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        nodoPrevio.Siguiente = nodoActual.Siguiente;
+                        Auxiliar = nodoActual.ObjetoConDatos;
+                        nodoActual = null;
+                        return Auxiliar;
+                    }
+                    else
+                    {
+                        nodoPrevio = nodoActual;
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                } while (nodoActual != null);
+
+                throw new Exception("No existe en la lista");
+
+            }
+        }
+        public Tipo Pop()
+        {
+            if (Vacia)
+            {
+                throw new Exception("La Pila esta Vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                nodoActual = Top;
+                Tipo Auxiliar;
+                Auxiliar = nodoActual.ObjetoConDatos;
+                Top = nodoActual.Siguiente;
+                nodoActual = null;
+                return Auxiliar;
+            }
+        }
+        public Tipo BuscarNodo(Tipo objeto)
+        {
+            if (Vacia)
+            {
+                throw new Exception("La lista se enceuntra vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                Tipo Auxiliar;
+                nodoActual = Top;
+                nodoPrevio = Top;
+                do
+                {
+                    if (objeto.Equals(Top.ObjetoConDatos))
+                    {
+                        Auxiliar = nodoActual.ObjetoConDatos;
+                        return Auxiliar;
+                    }
+                    if (objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        Auxiliar = nodoActual.ObjetoConDatos;
+                        return Auxiliar;
+                    }
+                    else
+                    {
+                        nodoPrevio = nodoActual;
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                } while (nodoActual != null);
+                throw new Exception("No existe en la lista");
+            }
+        }
+        public IEnumerator<Tipo> GetEnumerator()
+        {
+            if (Vacia)
+            {
+                yield break;
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                nodoActual = Top;
+                do
+                {
+                    yield return (nodoActual.ObjetoConDatos);
+                    nodoActual = nodoActual.Siguiente;
+                } while (nodoActual != null);
+
+                yield break;
+            }
+        }
+        public void Vaciar()
+        {
+            if (Vacia)
+            {
+                throw new Exception("La liste esta vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                nodoActual = Top;
+
+                do
+                {
+                    nodoPrevio = nodoActual;
+                    nodoActual = nodoActual.Siguiente;
+                    nodoPrevio = null;
+                } while (nodoActual != null);
+                Top = null;
+            }
+        }
+        ~ClasePilaDinamica()
+        {
+            Vaciar();
+        }
+    }
+}
