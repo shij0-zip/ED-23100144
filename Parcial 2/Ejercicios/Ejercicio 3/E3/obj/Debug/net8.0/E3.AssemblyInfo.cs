2bfa6f9e5cfbe4084741fb70aa283d7254409c92
shij0-zip <josueal.cass@gmail.com>
2025-11-21 20:16:15 -0600

    new update lol

--- Parcial 2/Ejercicios/Ejercicio 3/E3/obj/Debug/net8.0/E3.AssemblyInfo.cs
+++ Parcial 2/Ejercicios/Ejercicio 3/E3/obj/Debug/net8.0/E3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57cd62d0923a30b8723f7c30dbe214b85a73ae1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7f68c5b7686ddb20849944e3c916321ba181d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("E3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
